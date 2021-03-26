8539eaf9f5c22e21ce051c76554e70af44b47e4a
moonamansoor06 <moonamansoor06@gmail.com>
2021-03-26 12:03:11 +0500

    theme-ui added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Container,Heading,Button,Flex} from 'theme-ui';
+
+
 
 const Index = () => {
     return (
-        <div>
-            <h1>jamstack todo</h1>
-        </div>
+        <Container>
+            <Flex sx={{flexDirection:"column",padding:3}}>
+            <Heading as="h1"> Keep Track of your Todos</Heading>
+            <Button
+            sx={{marginTop:2}}
+            onClick={()=>{
+               alert('button clicked')
+            }}
+            >
+                Log in</Button>
+        </Flex>
+        </Container>
     )
 }
 export default Index
